7e9e1ee06f2f6af05b50ed8de92bf0624c5fa92f
Brian Takita <brian.takita@gmail.com>
2009-06-02 22:19:45 -0700

    Changed string name.

--- lib/lucky_luciano/resource.rb
+++ lib/lucky_luciano/resource.rb
@@ -6,11 +6,11 @@ module LuckyLuciano
       end
 
       ["get", "put", "post", "delete"].each do |http_verb|
-        class_eval(<<-EVAL, __FILE__, __LINE__ + 1)
+        class_eval(<<-RUBY, __FILE__, __LINE__ + 1)
         def #{http_verb}(relative_path, opts={}, &block)
           recorded_http_handlers << [:#{http_verb}, relative_path, opts, block]
         end
-        EVAL
+        RUBY
       end
     end
 
